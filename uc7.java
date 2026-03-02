9abd430e8715ecc8f6072f7a22cad14361322eae
Tarushi-123 <tarushitainwala@gmail.com>
2026-03-02 22:22:47 +0530

    uc7 completed

---
+++ uc7.java
@@ -0,0 +1,100 @@
+public class uc7 {
+
+    static class CharacterPatternMap {
+        private char character;
+        private String[] pattern;
+
+        public CharacterPatternMap(char character, String[] pattern) {
+            this.character = character;
+            this.pattern = pattern;
+        }
+
+        public char getCharacter() {
+            return character;
+        }
+
+        public String[] getPattern() {
+            return pattern;
+        }
+    }
+
+    public static CharacterPatternMap[] createCharacterPatternMaps() {
+
+        String[] O = {
+            " ***** ",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            " ***** "
+        };
+
+        String[] P = {
+            " ******",
+            "*     *",
+            "*     *",
+            " ******",
+            "*      ",
+            "*      ",
+            "*      "
+        };
+
+        String[] S = {
+            " ******",
+            "*      ",
+            "*      ",
+            " ***** ",
+            "      *",
+            "      *",
+            " ******"
+        };
+
+        String[] SPACE = {
+            "   ",
+            "   ",
+            "   ",
+            "   ",
+            "   ",
+            "   ",
+            "   "
+        };
+
+        return new CharacterPatternMap[] {
+            new CharacterPatternMap('O', O),
+            new CharacterPatternMap('P', P),
+            new CharacterPatternMap('S', S),
+            new CharacterPatternMap(' ', SPACE)
+        };
+    }
+
+    public static String[] getCharacterPattern(char ch, CharacterPatternMap[] charMaps) {
+        for (CharacterPatternMap map : charMaps) {
+            if (map.getCharacter() == ch) {
+                return map.getPattern();
+            }
+        }
+        return null; 
+    }
+
+    public static void printMessage(String message, CharacterPatternMap[] charMaps) {
+
+        for (int row = 0; row < 7; row++) {
+            StringBuilder line = new StringBuilder();
+
+            for (char ch : message.toCharArray()) {
+                String[] pattern = getCharacterPattern(ch, charMaps);
+                if (pattern != null) {
+                    line.append(pattern[row]).append("  ");
+                }
+            }
+            System.out.println(line);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        CharacterPatternMap[] charMaps = createCharacterPatternMaps();
+        printMessage("OOPS", charMaps);
+    }
+}
